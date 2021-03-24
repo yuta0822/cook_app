c8a538e73177a4d949e3bacc5abc3b8fa2c846b3
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-24 16:35:31 +0900

    cooks マイグレーション設定

---
+++ db/migrate/20210324071710_create_cooks.rb
@@ -0,0 +1,16 @@
+class CreateCooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cooks do |t|
+
+      t.string :title,            null: false 
+      t.text   :catch_copy,       null: false
+      t.integer :category_id,     null: false
+      t.integer :time_id,         null: false
+      t.text   :material,         null: false
+      t.text   :making,           null: false
+      t.text   :point
+      t.references :user,   foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_061637) do
+ActiveRecord::Schema.define(version: 2021_03_24_071710) do
+
+  create_table "cooks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.integer "category_id", null: false
+    t.integer "time_id", null: false
+    t.text "material", null: false
+    t.text "making", null: false
+    t.text "point"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cooks_on_user_id"
+  end
 
   create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -32,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_03_24_061637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cooks", "users"
 end
