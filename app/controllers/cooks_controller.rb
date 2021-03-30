0e8aceb257b1348364d0793961b3486becda4797
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-30 14:57:26 +0900

    検索結果機能実装

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,6 +1,6 @@
 class CooksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   
   def index
@@ -45,7 +45,11 @@ class CooksController < ApplicationController
   def show
     @cook = Cook.find(params[:id])
     @comments = @cook.comments.includes(:user)
-    @comment = Comment.new
+    @comment = Comment.new  
+  end
+
+  def search
+    @cook = Cook.search(params[:keyword])
   end
   
 
